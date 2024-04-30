237c44046e17274f7424499f0a21940c2aed1505
Pen179 <166961487+Pen179@users.noreply.github.com>
2024-04-30 00:46:35 -0700

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -1,1 +1,38 @@
+#include "Engine.h"
+using namespace std;
+using namespace sf;
 
+Engine::Engine()
+{
+	m_Window.create();
+
+	int pixelWidth = sf::VideoMode::getDesktopMode().width;
+	int pixelHeight = sf::VideoMode::getDesktopMode().height;
+	VideoMode vm(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height);
+	RenderWindow window(vm, "Particles", Style::Default);
+
+	View view;
+	view.setSize(pixelWidth, pixelHeight);
+	view.setCenter(pixelWidth / 2, pixelHeight / 2);
+	window.setView(view);
+}
+
+void Engine::run()
+{
+	Particle local_particle;
+}
+
+void Engine::input()
+{
+
+}
+
+void Engine::update(float drAsSeconds)
+{
+
+}
+
+void Engine::draw()
+{
+
+}
